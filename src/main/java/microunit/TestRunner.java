22bf0db979a8509b7324004184cbb56ad0b62414
PeterKelemen2 <peter.kelemen456@gmail.com>
2023-03-09 15:29:20 +0100

    Checkpoint

--- src/main/java/microunit/TestRunner.java
+++ src/main/java/microunit/TestRunner.java
@@ -1,8 +1,26 @@
 package microunit;
 
+import javax.print.attribute.standard.JobKOctets;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public abstract class TestRunner {
 
     protected Class<?> testClass;
     // csak a saját classa és aki örökli a classt látja
 
+    public TestRunner (Class<?> testClass){
+        this.testClass = testClass;
+    }
+
+    protected List<Method> getAnnotatedMethods(Class<? extends Annotation> annotationClass){
+        return Arrays.stream(testClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(annotationClass))
+                .toList();
+    }
+
+    protected abstract void invokeTestMethod(Method method, Object instance) throws IllegalAccessException;
 }
